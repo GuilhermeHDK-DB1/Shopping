9e88bcfa11cf33375dec962ca8d435508732429d
Guilherme Hideki Shibukawa <guilherme.shibukawa@db1.com.br>
2025-02-16 19:31:51 -0300

    criado seed na tabela de produtos

--- Shopping.ProductAPI/Migrations/20250216221354_SeedProducts.Designer.cs
+++ Shopping.ProductAPI/Migrations/20250216221354_SeedProducts.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shopping.ProductAPI.Model.Context;
 
@@ -10,9 +11,10 @@ using Shopping.ProductAPI.Model.Context;
 namespace Shopping.ProductAPI.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250216221354_SeedProducts")]
+    partial class SeedProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
