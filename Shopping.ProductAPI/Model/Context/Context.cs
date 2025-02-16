1c5ed27737ab89ad0d57e113cee3e1deaa15bf57
Guilherme Hideki Shibukawa <guilherme.shibukawa@db1.com.br>
2025-02-16 18:07:40 -0300

    first commit

---
+++ Shopping.ProductAPI/Model/Context/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shopping.ProductAPI.Model.Context;
+
+public class Context : DbContext
+{
+    public Context()
+    { }
+
+    public Context(DbContextOptions<Context> options) : base(options)
+    { }
+    
+    public DbSet<Product> Products { get; set; }
+}
---
+++ Shopping.ProductAPI/Model/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Shopping.ProductAPI.Model.Base;
+
+namespace Shopping.ProductAPI.Model;
+
+[Table("product")]
+public class Product : BaseEntity
+{
+    [Column("name")]
+    [Required]
+    [StringLength(150)]
+    public String Name { get; set; }
+    
+    [Column("price")]
+    [Required]
+    [Range(1, 10000)]
+    public Decimal Price { get; set; }
+    
+    [Column("description")]
+    [StringLength(500)]
+    public String Description { get; set; }
+    
+    [Column("category_name")]
+    [StringLength(50)]
+    public String CategoryName { get; set; }
+    
+    [Column("image_url")]
+    [StringLength(300)]
+    public String ImageUrl { get; set; }
+}
