a21d571a542bb9da23742bf2fc6fe3fd549f3a4a
Guilherme Hideki Shibukawa <guilherme.shibukawa@db1.com.br>
2025-02-22 17:43:19 -0300

    Listegem de itens

---
+++ Shopping.web/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.web.Services.IServices;
+
+namespace Shopping.web.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public async Task<IActionResult> ProductIndex()
+    {
+        var products = await _productService.FindAllAsync();
+        return View(products);
+    }
+}
